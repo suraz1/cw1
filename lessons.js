2aec96ca14ef4f83480c08de68836c09b43873c8
Suraj Bashyal <surajbashyal196@gmail.com>
2022-07-12 05:23:28 +0100

    added images to lesson.js JSON array

---
+++ lessons.js
@@ -0,0 +1,92 @@
+const lessons = [
+    {
+      id: 1,
+      subject: 'English',
+      location: 'Cambridge',
+      spaces: 5,
+      price: 500,
+      stars: 4,
+      image: 'images/English.png'
+    },
+    {
+      id: 2,
+      subject: 'Science',
+      location: 'London',
+      spaces: 5,
+      price: 350,
+      stars: 4,
+      image: 'images/science.png'
+    },
+    {
+      id: 3,
+      subject: 'Networking',
+      location: 'New York',
+      spaces: 5,
+      price: 750,
+      stars: 5,
+      image: 'images/networking.png'
+    },
+    {
+      id: 4,
+      subject: 'Music',
+      location: 'France',
+      spaces: 5,
+      price: 600,
+      stars: 4,
+      image: 'images/music.png'
+    },
+    {
+      id: 5,
+      subject: 'Mathematics',
+      location: 'Australia',
+      spaces: 5,
+      price: 250,
+      stars: 3,
+      image: 'images/mathematics.png'
+    },
+    {
+      id: 6,
+      subject: 'Literature',
+      location: 'Edinburgh',
+      spaces: 5,
+      price: 450,
+      stars: 2,
+      image: 'images/Literature.png'
+    },
+    {
+      id: 7,
+      subject: 'Hotel management',
+      location: 'South Korea',
+      spaces: 5,
+      price: 150,
+      stars: 3,
+      image: 'images/Hm.png'
+    },
+    {
+      id: 8,
+      subject: 'Geography',
+      location: 'Egypt',
+      spaces: 5,
+      price: 50,
+      stars: 4,
+      image: 'images/geography.png'
+    },
+    {
+      id: 9,
+      subject: 'Dance',
+      location: 'California',
+      spaces: 5,
+      price: 150,
+      stars: 5,
+      image: 'images/Dance.png'
+    },
+    {
+      id: 10,
+      subject: 'Arts',
+      location: 'Italy',
+      spaces: 5,
+      price: 200,
+      stars: 3,
+      image: 'images/art.png'
+    }
+  ]
